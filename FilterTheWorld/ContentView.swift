2423f264b9e1c048a6ac42d46ba8ff90a629bed3
akatzfey <katzfeyalex@gmail.com>
2022-04-03 16:00:33 -0700

    Initial Commit

---
+++ FilterTheWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FilterTheWorld
+//
+//  Created by Alexander Katzfey on 4/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilterTheWorld/FilterTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterTheWorldApp.swift
+//  FilterTheWorld
+//
+//  Created by Alexander Katzfey on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FilterTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
